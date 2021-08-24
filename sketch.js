e84f3badafc72de3e9342c3e02b1b60d5317092e
Mortex1 <88833704+Mortex1@users.noreply.github.com>
2021-08-24 17:03:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,49 +1,48 @@
-
-var ship, ship_running, edges;
-var seaImage;
+var trex, trex_running, edges;
+var groundImage;
 
 function preload(){
-  ship_running = loadAnimation("ship-2.png","ship-3.png","ship-4.png");
-  seaImage = loadImage("sea.png");
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  groundImage = loadImage("ground2.png")
 }
 
 function setup(){
-  createCanvas(600,600);
+  createCanvas(600,200);
   
- 
-  ship = createSprite(150,160,120,150);
-  ship.addAnimation("running", ship_running);
+  // creating trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
   edges = createEdgeSprites();
   
-  
-  //ship.scale = 0.5;
-  ship.x = 150
-sea=createSprite(200,180,400,20);
-sea.addImage("running",seaImage);
-sea.x=sea.width/2;
+  //adding scale and position to trex
+  trex.scale = 0.5;
+  trex.x = 50
+ground=createSprite(200,180,400,20);
+ground.addImage("running",groundImage);
+ground.x=ground.width/2;
 
 
 }
 
 
 function draw(){
-   
-  background("blue");
-  if(sea.x<0){
-    sea.x=sea.width/2;
+  //set background color 
+  background("white");
+  if(ground.x<0){
+    ground.x=ground.width/2;
 
     }
+  //logging the y position of the trex
+  console.log(trex.y)
   
-  console.log(ship.x)
-  
-  
-  if(keyDown("right")){
-    //ship.velocityX = 10;
+  //jump when space key is pressed
+  if(keyDown("space")){
+    trex.velocityY = -10;
   }
   
-  //ship.velocityX = ship.velocityX + 0.5;
+  trex.velocityY = trex.velocityY + 0.5;
   
- 
-  ship.collide(edges[3])
+  //stop trex from falling down
+  trex.collide(edges[3])
   drawSprites();
 }
